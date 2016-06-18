19968228ac51047108c2d019d394c1e5df791e2c
TxsAdamWest <Twinadanz@gmail.com>
2016-06-18 16:17:00 -0500

    styles improved

--- js/script.js
+++ js/script.js
@@ -226,7 +226,47 @@ hourlyButtonEl.addEventListener('click', handleForecastTypeClick)
 dailyButtonEl.addEventListener('click', handleForecastTypeClick)
 searchBarEl.addEventListener('keydown', searchByCity)
 
+//Locate by City
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+  }
+
+  function codeLatLng(lat, lng) {
+
+    var latlng = new google.maps.LatLng(lat, lng);
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+      console.log(results)
+        if (results[1]) {
+         //formatted address
+         alert(results[0].formatted_address)
+        //find country name
+             for (var i=0; i<results[0].address_components.length; i++) {
+            for (var b=0;b<results[0].address_components[i].types.length;b++) {
+
+            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                    //this is the object you are looking for
+                    city= results[0].address_components[i];
+                    break;
+                }
+            }
+        }
+        //city data
+        alert(city.short_name + " " + city.long_name)
+
+
+        } else {
+          alert("No results found");
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
+  }
+
 //4 - Add event listner to page to listen for the hash change, once it does it will invoke router.
 //Homepage Loader
 window.addEventListener('hashchange', router)
 router()
+initialize()
